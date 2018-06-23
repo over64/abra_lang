29783424b4d4017507545b7b1a3b25dd0cddec6d
over <over@ws0.over64>
2018-06-23 13:09:54 +0600

    test

--- abra_v2/src/test/scala/integration/_24Collections.scala
+++ abra_v2/src/test/scala/integration/_24Collections.scala
@@ -1,5 +1,22 @@
 package integration
 
-class _24Collections {
+import org.scalatest.FunSuite
 
+class _24Collections  extends FunSuite with IntegrationUtil {
+  test("array seq") {
+    assertCodeEquals(
+      """
+        import
+          /universe with None, Int, String
+          /array
+          /iter .
+
+        def main =
+          array1 = array.mk[Int](5, lambda i: Int -> i)
+          it = array1.map(lambda x: Int -> x * 2).iterator()
+          it.next()
+          it.next()
+          it.next() .
+  """, exit = Some(4))
+  }
 }
